bfc2b0a564b352edad461bfb0c36520bf12eb08f
cankutkostur <kostur15@itu.edu.tr>
2018-11-10 12:46:29 +0300

    Update server.py
    heroku connection test

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello, world! Heroku connection test."
+    return "Hello, world! Heroku test."
 
 
 if __name__ == "__main__":
